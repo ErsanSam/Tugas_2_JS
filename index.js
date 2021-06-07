43a36094c8d7ef85e51bc51dfee653a8f97333a0
Ersan Samjaya <fernandoersan28@gmail.com>
2021-06-07 16:10:47 +0700

    Tugas 2 JS
    Tugas ini saya kumpulkan untuk NIOMIC

---
+++ index.js
@@ -0,0 +1,17 @@
+var nama = "Ersan Fernando Samjaya";
+let umur = 19;
+var tinggi = false;
+var cm = 180;
+if(cm>170)
+tinggi = true;
+else
+tinggi = false;
+var rumah = "kontrakan";
+rumah = null;
+var sekolah;
+
+console.log(nama)
+console.log(umur)
+console.log(tinggi)
+console.log(rumah)
+console.log(sekolah)
